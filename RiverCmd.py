be9352eb6dfa6446310c7d79db26a884fa3dd02f
Neerav Nagda <neerav24@hotmail.com>
2017-11-28 14:25:22 +0000

    Command plugin to create a river node

---
+++ RiverCmd.py
@@ -0,0 +1,133 @@
+## RiverCmd.py
+# This command is used to create a river node and lofted surface
+
+import sys
+import maya.api.OpenMaya as om
+
+#----------------------------------------------------------
+# Plugin
+#----------------------------------------------------------
+
+# The name of the command
+kPluginCmdName = "createRiver"
+
+# Flag details
+shortFlagNames = ["-d","-w","-n"]
+longFlagNames = ["-depth","-width","-name"]
+
+class RiverCmdClass(om.MPxCommand):
+
+	## Constructor
+	def __init__(self):
+		om.MPxCommand.__init__(self)
+
+	## This function is used to indicate the command is undoable
+	def isUndoable(self):
+		return True
+
+	## The doIt function
+	def doIt(self, args):
+		# Initialise values
+		self.depthValue = 1.0
+		self.widthValue = 1.0
+		self.name = "RiverNode"
+		self.curve = None
+		self.parseArguments(args)
+		self.redoIt()
+
+	def redoIt(self):
+		# Create a dg modifier
+		dgModifier = om.MDGModifier()
+		# Create the river node
+		self.riverNode = dgModifier.createNode("RiverNode")
+		dgModifier.renameNode(self.riverNode, self.name)
+		# Create the loft node
+		self.loftNode = dgModifier.createNode("loft")
+		dgModifier.renameNode(self.loftNode, self.name + "Loft")
+		# Execute the dg modifier queue
+		dgModifier.doIt()
+
+	def undoIt(self):
+		# Create a dg modifier
+		dgModifier = om.MDGModifier()
+		dgModifier.deleteNode(self.riverNode)
+		dgModifier.deleteNode(self.loftNode)
+		dgModifier.doIt()
+
+	## Parse the arguments and flags
+	def parseArguments(self, args):
+		argData = om.MArgParser(self.syntax(), args)
+		# If an argument exists, it will be the curve name. So it gets selected
+		try:
+			curveName = argData.commandArgumentString(0)
+			selectionList = om.MGlobal.getSelectionListByName(curveName)
+		except:
+			selectionList = om.MGlobal.getActiveSelectionList()
+		self.curve = self.findCurveFromSelection(selectionList)
+		# Parse the flags
+		if argData.isFlagSet("-d"):
+			self.depthValue = argData.flagArgumentFloat("-d",0)
+		if argData.isFlagSet("-depth"):
+			self.depthValue = argData.flagArgumentFloat("-depth",0)
+		if argData.isFlagSet("-w"):
+			self.widthValue = argData.flagArgumentFloat("-w",0)
+		if argData.isFlagSet("-width"):
+			self.widthValue = argData.flagArgumentFloat("-width",0)
+		if argData.isFlagSet("-n"):
+			self.name = argData.flagArgumentFloat("-n",0)
+		if argData.isFlagSet("-name"):
+			self.name = argData.flagArgumentFloat("-name",0)
+
+	## Find the curve from the selection list
+	def findCurveFromSelection(self, selectionList):
+		iterator = om.MItSelectionList(selectionList, om.MFn.kDagNode)
+		# Check if nothing is selected
+		if iterator.isDone():
+			print "Error nothing selected"
+			return None
+		else:
+			dagPath = om.MDagPath()
+			dagFn = om.MFnDagNode()
+			# Get the first item in the iterator
+			dagPath = iterator.getDagPath()
+			try:
+				dagPath.extendToShape()
+			except:
+				pass
+			dagObject = dagPath.node()
+			dagFn.setObject(dagObject)
+			name = dagFn.name()
+		return name
+
+## Tell Maya to use Python API 2.0
+def maya_useNewAPI():
+	pass
+
+## Create an instance of the command
+def cmdCreator():
+	return RiverCmdClass()
+
+## Define the argument and syntax for the command
+def syntaxCreator():
+	syntax = om.MSyntax()
+	syntax.addArg(om.MSyntax.kString)
+	syntax.addFlag(shortFlagNames[0], longFlagNames[0], om.MSyntax.kDouble)
+	syntax.addFlag(shortFlagNames[1], longFlagNames[1], om.MSyntax.kDouble)
+	syntax.addFlag(shortFlagNames[2], longFlagNames[2], om.MSyntax.kString)
+	return syntax
+
+## Initialise the plugin when Maya loads it
+def initializePlugin(mobject):
+	mplugin = om.MFnPlugin(mobject)
+	try:
+		mplugin.registerCommand(kPluginCmdName, cmdCreator, syntaxCreator)
+	except:
+		sys.stderr.write("Failed to register command: " + kPluginCmdName)
+
+## Uninitialise the plugin when Maya unloads it
+def uninitializePlugin(mobject):
+	mplugin = om.MFnPlugin(mobject)
+	try:
+		mplugin.deregisterCommand(kPluginCmdName)
+	except:
+		sys.stderr.write("Failed to unregister command: " + kPluginCmdName)
