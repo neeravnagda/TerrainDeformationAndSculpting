0a6c0979ae9192926cfbcce1c595bc082f108b52
Nagda Neerav <i7421407@wg0626.student.bournemouth.ac.uk>
2017-12-03 18:00:57 +0000

    Working HeightField node

--- HeightFieldNode/src/HeightFieldNode.cpp
+++ HeightFieldNode/src/HeightFieldNode.cpp
@@ -1,4 +1,5 @@
 #include <maya/MDataHandle.h>
+#include <maya/MFnEnumAttribute.h>
 #include <maya/MFnData.h>
 #include <maya/MFnMesh.h>
 #include <maya/MFnMeshData.h>
@@ -14,10 +15,17 @@
 
 //-----------------------------------------------------------------------------
 // Set static members
-MTypeID HeightField::m_id(0x1003);
+MTypeId HeightField::m_id(0x1003);
 const MString HeightField::typeName("HeightFieldNode");
 MObject HeightField::m_inMesh;
+MObject HeightField::m_spaceType;
+MObject HeightField::m_noiseType;
 MObject HeightField::m_amplitude;
+MObject HeightField::m_seed;
+MObject HeightField::m_frequency;
+MObject HeightField::m_fractalOctaves;
+MObject HeightField::m_lacunarity;
+MObject HeightField::m_fractalGain;
 MObject HeightField::m_outMesh;
 //-----------------------------------------------------------------------------
 void* HeightField::creator()
@@ -30,122 +38,195 @@ MStatus HeightField::initialize()
 	MStatus stat;
 	MFnTypedAttribute typedAttr;
 	MFnNumericAttribute numericAttr;
+    MFnEnumAttribute enumAttr;
 
 	// Input node attributes
-	m_inMesh = typedAttr.create("inMesh", "im", MFnData::kMesh, &stat);
-	if (!stat)
-		return stat;
+    m_inMesh = typedAttr.create("inMesh", "im", MFnData::kMesh);
 	typedAttr.setReadable(false);
 	typedAttr.setWritable(true);
 	typedAttr.setStorable(true);
+    stat = addAttribute(m_inMesh);
+    if (!stat)
+        return stat;
 
-	m_amplitude = numericAttr.create("amplitude", "a", MFnNumericData::kFloat, 1.0, &stat);
-	if (!stat)
-		return stat;
+    // Default value = 5, which is SimplexFractal
+    m_noiseType = enumAttr.create("noiseType", "nt", 5);
+    enumAttr.addField("Simplex", FastNoise::SimplexFractal);
+    enumAttr.addField("Perlin", FastNoise::PerlinFractal);
+    enumAttr.addField("Cubic", FastNoise::CubicFractal);
+    stat = addAttribute(m_noiseType);
+    if (!stat)
+        return stat;
+
+    // Default value = 2, which is object space
+    m_spaceType = enumAttr.create("spaceType", "st", 2);
+    enumAttr.addField("Object", MSpace::kObject);
+    enumAttr.addField("World", MSpace::kWorld);
+    stat = addAttribute(m_spaceType);
+    if (!stat)
+        return stat;
+
+    m_amplitude = numericAttr.create("amplitude", "a", MFnNumericData::kFloat, 1.0);
 	numericAttr.setReadable(false);
 	numericAttr.setWritable(true);
 	numericAttr.setStorable(true);
+    stat = addAttribute(m_amplitude);
+    if (!stat)
+        return stat;
 
-	// Output node attributes
-	m_outMesh = typedAttr.create("outMesh", "om", MFnData::kMesh, &stat);
-	if (!stat)
-		return stat;
+    // Note 1337 is the default seed from FastNoise
+    m_seed = numericAttr.create("seed", "s", MFnNumericData::kInt, 1337);
+    numericAttr.setReadable(false);
+    numericAttr.setWritable(true);
+    numericAttr.setStorable(true);
+    stat = addAttribute(m_seed);
+    if (!stat)
+        return stat;
+
+    m_frequency = numericAttr.create("frequency", "f", MFnNumericData::kFloat, 0.01f);
+    numericAttr.setReadable(false);
+    numericAttr.setWritable(true);
+    numericAttr.setStorable(true);
+    stat = addAttribute(m_frequency);
+    if (!stat)
+        return stat;
+
+    m_fractalOctaves = numericAttr.create("fractalOctaves", "fo", MFnNumericData::kInt, 8);
+    numericAttr.setReadable(false);
+    numericAttr.setWritable(true);
+    numericAttr.setStorable(true);
+    stat = addAttribute(m_fractalOctaves);
+    if (!stat)
+        return stat;
+
+    m_lacunarity = numericAttr.create("lacunarity", "l", MFnNumericData::kFloat, 2.0f);
+    numericAttr.setReadable(false);
+    numericAttr.setWritable(true);
+    numericAttr.setStorable(true);
+    stat = addAttribute(m_lacunarity);
+    if (!stat)
+        return stat;
+
+    m_fractalGain = numericAttr.create("fractalGain", "fg", MFnNumericData::kFloat, 0.5);
+    numericAttr.setReadable(false);
+    numericAttr.setWritable(true);
+    numericAttr.setStorable(true);
+    stat = addAttribute(m_fractalGain);
+    if (!stat)
+        return stat;
+
+    // Output node attributes
+    m_outMesh = typedAttr.create("outMesh", "om", MFnData::kMesh);
 	typedAttr.setReadable(true);
 	typedAttr.setWritable(false);
 	typedAttr.setStorable(false);
-
-	// Add the attributes to the class
-	stat = addAttribute(m_inMesh);
-	if (!stat)
-		return stat;
-	stat = addAttribute(m_amplitude);
-	if (!stat)
-		return stat;
 	stat = addAttribute(m_outMesh);
 	if (!stat)
 		return stat;
 
 	// Connect input/output dependencies
 	attributeAffects(m_inMesh, m_outMesh);
+    attributeAffects(m_spaceType, m_outMesh);
+    attributeAffects(m_noiseType, m_outMesh);
 	attributeAffects(m_amplitude, m_outMesh);
+    attributeAffects(m_seed, m_outMesh);
+    attributeAffects(m_frequency, m_outMesh);
+    attributeAffects(m_fractalOctaves, m_outMesh);
+    attributeAffects(m_lacunarity, m_outMesh);
+    attributeAffects(m_fractalGain, m_outMesh);
 
 	// Return success
 	return MStatus::kSuccess;
 }
 //-----------------------------------------------------------------------------
-void HeightField::compute(const MPlug &_plug, MDataBlock &_data)
+MStatus HeightField::compute(const MPlug &_plug, MDataBlock &_data)
 {
 	MStatus stat;
 	// Check if the plug is the output plug
 	if (_plug == m_outMesh)
 	{
-		// Get the data handle for the mesh input
-		MDataHandle inMeshDataHandle = _data.inputValue(m_inMesh, &stat);
-		if (!stat)
-			return stat;
-		// Get the input mesh as a MObject
+        // Get the input data handles and typecast
+        MDataHandle inMeshDataHandle = _data.inputValue(m_inMesh);
 		MObject inMeshValue = inMeshDataHandle.asMesh();
 
-		// Get the data handle for the amplitude input
-		MDataHandle inAmplitudeDataHandle = _data.inputValue(m_amplitude, &stat);
-		if (!stat)
-			return stat;
-		// Get the amplitude as a float
-		float amplitudeValue = inAmplitudeDataHandle.asFloat();
+        MDataHandle spaceTypeDataHandle = _data.inputValue(m_spaceType);
+        MSpace::Space spaceTypeValue = (MSpace::Space)spaceTypeDataHandle.asShort();
 
-		// Get the data handle for the output value
-		MDataHandle outMeshDataHandle = _data.outputValue(m_outMesh, &stat);
-		if (!stat)
-			return stat;
+        MDataHandle noiseTypeDataHandle = _data.inputValue(m_noiseType);
+        FastNoise::NoiseType noiseTypeValue = (FastNoise::NoiseType)noiseTypeDataHandle.asShort();
+
+        MDataHandle amplitudeDataHandle = _data.inputValue(m_amplitude);
+        float amplitudeValue = amplitudeDataHandle.asFloat();
+
+        MDataHandle seedDataHandle = _data.inputValue(m_seed);
+        int seedValue = seedDataHandle.asInt();
+
+        MDataHandle frequencyDataHandle = _data.inputValue(m_frequency);
+        float frequencyValue = frequencyDataHandle.asFloat();
+
+        MDataHandle fractalOctavesDataHandle = _data.inputValue(m_fractalOctaves);
+        int fractalOctavesValue = fractalOctavesDataHandle.asInt();
+
+        MDataHandle lacunarityDataHandle = _data.inputValue(m_lacunarity);
+        float lacunarityValue = lacunarityDataHandle.asFloat();
+
+        MDataHandle fractalGainDataHandle = _data.inputValue(m_fractalGain);
+        float fractalGainValue = fractalGainDataHandle.asFloat();
+
+        // Get the data handle for the output value
+        MDataHandle outMeshDataHandle = _data.outputValue(m_outMesh);
 
 		// Set FastNoise parameters
-		m_fastNoise.SetNoiseType(FastNoise::SimplexFractal);
+        m_fastNoise.SetNoiseType(noiseTypeValue);
+        // Only set the seed if it has changed
+        if (m_fastNoise.GetSeed() != seedValue)
+            m_fastNoise.SetSeed(seedValue);
+        m_fastNoise.SetFrequency(frequencyValue);
+        m_fastNoise.SetFractalOctaves(fractalOctavesValue);
+        m_fastNoise.SetFractalLacunarity(lacunarityValue);
+        m_fastNoise.SetFractalGain(fractalGainValue);
 
 		// Create a function set for the input mesh
-		MFnMesh inMeshFn(inMeshValue, &stat);
-		if (!stat)
-			return stat;
+        MFnMesh inMeshFn(inMeshValue);
+
 		// Create a mesh data object and a new MObject
-		MFnMeshData meshData;
-		MObject meshDataObj = meshData.create(&stat);
-		if (!stat)
-			return stat;
+        MFnMeshData meshDataFn;
+        MObject outMeshObj = meshDataFn.create();
 		// Create a function set for the new mesh
 		MFnMesh newMeshFn;
-		MObject outMesh = newMeshFn.copy(inMeshValue, meshDataObj, &stat);
-		if (!stat)
-			return stat;
+        newMeshFn.copy(inMeshValue, outMeshObj);
 
 		// Get the vertices of the original mesh
 		MPointArray vertices;
-		stat = inMeshFn.getPoints(&vertices, MSpace::kObject);
-		if (!stat)
-			return stat;
+        stat = inMeshFn.getPoints(vertices, spaceTypeValue);
 
 		// Iterate through each point and adjust the y value
 		float height = 0.0f;
 		MVector heightDisplacement(0.0, 0.0, 0.0);
-		size_t numVertices = vertices.length();
+        size_t numVertices = vertices.length();
 		for (size_t i = 0; i < numVertices; ++i)
 		{
 			MPoint& currentPoint = vertices[i];
-			height = m_fastNoise.GetNoise(currentPoint.x, currentPoint.z);
+            height = m_fastNoise.GetNoise(currentPoint.x, currentPoint.z) * amplitudeValue;
 			heightDisplacement.y = height;
 			currentPoint += heightDisplacement;
 		}
 		// Set the output mesh vertices
-		stat = outMesh.setPoints(vertices, MSpace::kWorld);
-		if (!stat)
-			return stat;
+        newMeshFn.setPoints(vertices);
+
+        newMeshFn.setObject(outMeshObj);
 
 		// Set the output value
-		outMeshDataHandle.set(outMesh);
+        outMeshDataHandle.set(outMeshObj);
 
 		// Set the plug as clean
 		stat = _data.setClean(_plug);
 		if (!stat)
 			return stat;
+
+        return MStatus::kSuccess;
 	}
+    return MStatus::kUnknownParameter;
 }
 //-----------------------------------------------------------------------------
 HeightField::HeightField(){}
